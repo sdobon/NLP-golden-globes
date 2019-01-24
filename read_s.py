6836545fbcf4ece2f642012f2b9cf5218fb82087
sdobon <sebastiandobon2@gmail.com>
2019-01-24 13:08:00 -0600

    filter host, most frequent pns sd

--- read_s.py
+++ read_s.py
@@ -1,6 +1,7 @@
 import json
 from pprint import pprint
 import re, nltk
+from collections import Counter
 # m = re.search("Your number is <b>(\d+)</b>",
 #       "xxx Your number is <b>123</b>  fdjsk")
 # if m:
@@ -11,16 +12,31 @@ with open('gg2013.json') as f:
 
 tweets = []
 
-for d in data[0:5000]:
+for d in data:
     tweets.append(d['text'])
 
-pat = re.compile(" [Hh]ost")
+host_pat = re.compile(" [Hh]ost")
+pn2_pat= re.compile("[A-Z][a-z]+ [A-Z][a-z]+")
+wf = re.compile("Will Ferrell")
+
+# print(pn2_pat.findall("asdfasdfsd   ina Fey"))
 
 host = []
+all_proper_nouns = []
 
 for t in tweets:
-    if pat.search(t):
+    if host_pat.search(t):
         host.append(t)
+        if wf.search(t):
+            try:
+                print(t)
+            except:
+                pass
+        proper_nouns= pn2_pat.findall(t)
+        for n in proper_nouns:
+            all_proper_nouns.append(n)
 
-pprint(host)
+# print(all_proper_nouns)
+print(Counter(all_proper_nouns).most_common(10))
+# pprint(host)
 print(len(host))
