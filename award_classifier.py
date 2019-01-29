2b3222a6ce6c2ef73d92c10d20f57b121844f071
george.pan <georgepan98@gmail.com>
2019-01-28 21:02:53 -0600

    add award classifier with comments

--- award_classifier.py
+++ award_classifier.py
@@ -5,22 +5,21 @@ from nltk import word_tokenize
 from nltk.text import Text
 from pprint import pprint
 
-raw = "the project gutenberg ebook is available for free. the fork is in the raw chicken"
-
-tokens = word_tokenize(raw)
-
-#d = Text(raw)
-
 with open('gg2013.json') as f:
     data = json.load(f)
 
+# Call our corpus of tweets as texts
 texts = []
 for d in data:
     texts.append(d['text'])
 
 texts = list(set(texts))
-    
 
+# Remove punctuation from our corpus
+punctuation = re.compile(r'[^\w\s]')
+unpunctuated = [re.sub(punctuation,'',i) for i in texts]
+    
+# Helper function: input -> some array; output -> list of indices for array where entries are NOT None
 def index_return(array):
     listofindices = []
     for i in range(len(array)):
@@ -28,28 +27,33 @@ def index_return(array):
             listofindices.append(i)
     return listofindices
 
+# List of hard coded awards
 OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best motion picture - comedy or musical', 'best performance by an actress in a motion picture - comedy or musical', 'best performance by an actor in a motion picture - comedy or musical', 'best animated feature film', 'best foreign language film', 'best performance by an actress in a supporting role in a motion picture', 'best performance by an actor in a supporting role in a motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best television series - comedy or musical', 'best performance by an actress in a television series - comedy or musical', 'best performance by an actor in a television series - comedy or musical', 'best mini-series or motion picture made for television', 'best performance by an actress in a mini-series or motion picture made for television', 'best performance by an actor in a mini-series or motion picture made for television', 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television', 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television']
 
+# Make a set of all the words in OFFICIAL_AWARDS
 award_words = set()
 for i in OFFICIAL_AWARDS:
     for j in i.split():
         if len(j) <= 3:pass 
+        if j == "award": pass
         else:
             award_words.add(j)
             
-matches = [re.search('win|won', text) for text in texts]
-win_texts = []
-l = index_return(matches)
-for i in l:
-    win_texts.append(texts[i])
-matches = [re.search('best',text) for text in win_texts]
-l = index_return(matches)
-best_texts = []
-for i in l:
-    best_texts.append(win_texts[i])
+# COMMENTED OUT - finds all the tweets that mention ("win" OR "won") AND "best"
+#matches = [re.search('win|won', text) for text in texts]
+#win_texts = []
+#l = index_return(matches)
+#for i in l:
+#    win_texts.append(texts[i])
+#matches = [re.search('best',text) for text in win_texts]
+#l = index_return(matches)
+#best_texts = []
+#for i in l:
+#    best_texts.append(win_texts[i])
     
-test = best_texts[5]
-
+# Categorize: input -> a line of text (tweet)
+#             output -> a number which corresponds to the index of OFFICIAL_AWARDS
+#                       for example, 1 corresponds to OFFICIAL_AWARDS[1] or "best motion picture - drama"
 def categorize(text):
     award_words_in_test = []
     for i in text.split():
@@ -62,8 +66,8 @@ def categorize(text):
     for i in OFFICIAL_AWARDS:
         sim_count = 0
         words = set(i.split())
-        for i in award_words_in_test:
-            if i in words:
+        for j in award_words_in_test:
+            if j in words:
                 sim_count += 1
         count+=1
         if sim_count > max_sim_count:
@@ -71,15 +75,4 @@ def categorize(text):
             max_count = count
     return max_count - 1
 
-categories = [categorize(i) for i in best_texts]
-
-indices = []
-for i in range(len(categories)):
-    if categories[i] == 0:
-        indices.append(i)
-    
-for i in indices:
-    print(best_texts[i])
         
-
-
