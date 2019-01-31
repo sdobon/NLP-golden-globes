135e0452b58bd2d57868984f43926787dff258b3
sdobon <sebastiandobon2@gmail.com>
2019-01-31 17:11:19 -0600

    new pats

--- read_s.py
+++ read_s.py
@@ -5,9 +5,6 @@ from collections import Counter
 
 from answers import answers
 
-# from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
-# from sklearn.naive_bayes import GaussianNB, MultinomialNB, ComplementNB
-# import numpy as np
 
 with open('gg2013.json') as f:
     data = json.load(f)
@@ -22,40 +19,6 @@ OFFICIAL_AWARDS = ['cecil b. demille award', 'best motion picture - drama', 'bes
 
 
 
-
-# name_map = {
-# 'cecil b. demille award': 'cecil b demille',
-# 'best motion picture - drama': 'best drama',
-# 'best performance by an actress in a motion picture - drama': 'best actress drama',
-# 'best performance by an actor in a motion picture - drama': 'best actor drama',
-# 'best motion picture - comedy or musical': 'best comedy musical',
-# 'best performance by an actress in a motion picture - comedy or musical': 'best actress comedy musical',
-# 'best performance by an actor in a motion picture - comedy or musical': 'best actor comedy musical',
-# 'best animated feature film': 'best animated',
-# 'best foreign language film': 'best foreign',
-# 'best performance by an actress in a supporting role in a motion picture': 'best supporting actress',
-# 'best performance by an actor in a supporting role in a motion picture': 'best supporting actor',
-# 'best director - motion picture': 'best director',
-# 'best screenplay - motion picture': 'best screenplay',
-# 'best original score - motion picture': 'best score',
-# 'best original song - motion picture': 'best song',
-# 'best television series - drama': 'best tv drama',
-# 'best performance by an actress in a television series - drama': 'best actress tv drama',
-# 'best performance by an actor in a television series - drama': 'best actor tv drama',
-# 'best television series - comedy or musical': 'best tv comedy musical',
-# 'best performance by an actress in a television series - comedy or musical': 'best actress tv comedy musical',
-# 'best performance by an actor in a television series - comedy or musical': 'best actor tv comedy musical',
-# 'best mini-series or motion picture made for television': 'best tv',
-# 'best performance by an actress in a mini-series or motion picture made for television': 'best actress tv',
-# 'best performance by an actor in a mini-series or motion picture made for television': 'best actor tv',
-# 'best performance by an actress in a supporting role in a series, mini-series or motion picture made for television': 'best supporting acress tv',
-# 'best performance by an actor in a supporting role in a series, mini-series or motion picture made for television': 'best supporting actor tv'}
-
-# def generate_pat(award):
-#     s = ""
-#     for w in award.split():
-#         s += w + ".*"
-#     return s[:-2]
 
 #-----------------------------------------------------------------------------------------------
 
@@ -86,24 +49,24 @@ def classify(tweet):
     else:
         return None
 
-print(classify('best supporting actor tv'))
+# print(classify('best supporting actor tv'))
 
 
 #-----------------------------------------------------------------------------------------------
 
 
-# Make a set of all the words in OFFICIAL_AWARDS
-award_words = set()
-for a in OFFICIAL_AWARDS:
-    for w in tokenize(a):
-        if len(w) <= 3:pass
-        if w == "award": pass
-        else:
-            award_words.add(w)
-
-
-print(award_words)
-print(len(award_words))
+# # Make a set of all the words in OFFICIAL_AWARDS
+# award_words = set()
+# for a in OFFICIAL_AWARDS:
+#     for w in tokenize(a):
+#         if len(w) <= 3:pass
+#         if w == "award": pass
+#         else:
+#             award_words.add(w)
+#
+#
+# print(award_words)
+# print(len(award_words))
 
 
 #-----------------------------------------------------------------------------------------------
@@ -113,6 +76,9 @@ win_pat = re.compile("w[io]n")
 pn2_pat= re.compile("[A-Z][a-z]+ [A-Z][a-z]+")
 rt = re.compile("rt")
 award_related_pat = re.compile("best.*actress.*television.*drama")
+
+name_pattern = re.compile(r'\b[A-Z][a-z]+\b(?:\s+[A-Z][a-z]*\b)*')
+pnx_pat= re.compile(r'\b[A-Z]\S*\b(?:\s+[A-Z]\S*\b)*')
 
 win = []
 
@@ -130,7 +96,7 @@ all_win_pnouns = [[] for a in OFFICIAL_AWARDS]
 for t in win:
     award = classify(t)
     if award:
-        proper_nouns = pn2_pat.findall(t)
+        proper_nouns = pnx_pat.findall(t)
         for n in proper_nouns:
             all_win_pnouns[OFFICIAL_AWARDS.index(award)].append(n)
 
