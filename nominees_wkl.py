f81f95a3ac02aef5e49a70bc349b42bceef80b5a
willklundgren <willklundgren@gmail.com>
2019-02-17 15:07:21 -0600

    actor / actress awards for nominees

--- nominees_wkl.py
+++ nominees_wkl.py
@@ -1,10 +1,12 @@
 import json
 from pprint import pprint
-import re, nltk
+import re, nltk, imdb, sys
 from collections import Counter
 
 from answers import answers
 
+reload(sys)
+sys.setdefaultencoding('utf8')
 
 with open('gg2013.json') as f:
     data = json.load(f)
@@ -83,21 +85,57 @@ pn2_pat= re.compile("(?!Best)(?!Golden)(?!Globes)(?!Supporting)(?!Actor)(?!Actre
 rt = re.compile("rt")
 award_related_pat = re.compile("best.*actress.*television.*drama")
 act_pat = re.compile("act")
+director_pat = re.compile("direct")
+
+# Common nominee patterns for actors / actresses
+# Nominee ____
+nominee_pat_1 = re.compile("[Nn]ominee [[A-Z][a-z]+ [A-Z][a-z]+")
+# n = n[8:]
+# _____ was nominated
+nominee_pat_2 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ was nominated")
+# n = n[0:-14]
+# ___ was robbed
+nominee_pat_3 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ was robbed")
+# n = n[0:-11]
+# ____ didn't win
+nominee_pat_4 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ did not win|[A-Z][a-z]+ [A-Z][a-z]+ didn[']*t win")
+# I wanted ____
+nominee_pat_5 = re.compile("wanted [A-Z][a-z]+ [A-Z][a-z]+")
+# should have been ___
+nominee_pat_6 = re.compile("should have been [A-Z][a-z]+ [A-Z][a-z]+|should[']*ve been [A-Z][a-z]+ [A-Z][a-z]+")
+# ____ should have won
+nominee_pat_7 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ should have won|[A-Z][a-z]+ [A-Z][a-z]+ should[']*ve won")
+
+# Common nominee patterns for movie titles
+nominee_pat_8 = re.compile("[Nn]ominee [[A-Z][a-z]+ [A-Z][a-z]+|[Nn]ominee [A-Z][a-z]+.[a-z]{1,3}.[A-Z][a-z]+")
+# n = n[8:]
+# _____ was nominated
+nominee_pat_9 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ was nominated|[Nn]ominee [A-Z][a-z]+.[a-z]{1,3}.[A-Z][a-z]+")
+# n = n[0:-14]
+# ___ was robbed
+nominee_pat_10 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ was robbed")
+# n = n[0:-11]
+# ____ didn't win
+nominee_pat_11 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ did not win|[A-Z][a-z]+ [A-Z][a-z]+ didn[']*t win")
+# I wanted ____
+nominee_pat_12 = re.compile("wanted [A-Z][a-z]+ [A-Z][a-z]+")
+# should have been ___
+nominee_pat_13 = re.compile("should have been [A-Z][a-z]+ [A-Z][a-z]+|should[']*ve been [A-Z][a-z]+ [A-Z][a-z]+")
+# ____ should have won
+nominee_pat_14 = re.compile("[A-Z][a-z]+ [A-Z][a-z]+ should have won|[A-Z][a-z]+ [A-Z][a-z]+ should[']*ve won")
+
 
 name_pattern = re.compile(r'\b(?!Best)[A-Z][a-z]*\b(?:\s+[A-Z][a-z]*\b)*')
 name_with_lower = re.compile(r'\b(?!Best)[A-Z][a-z]*\b(?:\s+[a-z]*\b){1,2}(?:\s+[A-Z][a-z]*\b)+')
 pnx_pat= re.compile(r'\b[A-Z]\S+\b(?:\s+[A-Z]\S*\b)*')
 
-nominee_pat = re.compile("[Dd]idn't win|Did not win|didn't get|did not get|[Nn]ominee|[Nn]ominated|[Nn]omination")
-# was\s[Nn]ominated|get[Nn]omin"
-# for t in tweets:
-#     print t
-#     print("\n")
+nominee_pat = re.compile("[Dd]idn't win|Did not win|didn't get|did not get|[Nn]ominee|[Nn]ominated|[Nn]omination|robbed|wanted [A-Z][a-z]+|should have been|should've been|should've won|should have won")
 
 nominee = []
 
 #append anything matching nominee_pat to nominee[]
 for t in tweets:
+    # print t
     low = t.lower()
     if not rt.match(low):
         if (nominee_pat.search(low) or win_pat.search(low)):
@@ -109,53 +147,126 @@ for t in tweets:
 all_nominee_pnouns = [[] for a in OFFICIAL_AWARDS]
 #print all_nominee_pnouns
 
-# Return array of names only!
+# Return array of actor / actress names only!
 def find_names_only(list_of_pronouns):
+    # print ("Entering find_names_only")
+    actor = imdb.IMDb()
     names_array = []
     stringified_pronouns = [i.encode("utf-8") for i in list_of_pronouns]
     for pronoun in stringified_pronouns:
-        if ((nltk.pos_tag(nltk.word_tokenize(pronoun))[0][1] == "NNP") and (nltk.pos_tag(nltk.word_tokenize(pronoun))[1][1] == "NNP")):
-            names_array.append(pronoun)
+        # if ((nltk.pos_tag(nltk.word_tokenize(pronoun))[0][1] == "NNP") and (nltk.pos_tag(nltk.word_tokenize(pronoun))[1][1] == "NNP")):
+        #     names_array.append(pronoun)
+        try:
+            if (actor.search_movie(pronoun)[0]['name'] == pronoun):
+                names_array.append(pronoun)
+        except:
+            pass
+    # print "Returned list of names: ", names_array
     return list(set(names_array))
-
-# test_pronouns = [u'Is Ben Affleck', u'Matt Damon', u'Affleck', u'Picture', u'Director', u'Matt Damon', u'Affleck just won Best Picture', u'Director while Matt Damon']
-# print "CHECK (Matt Damon only): ", find_names_only(test_pronouns)
-
 
 # Classify the nominee-related tweets by their awards, and
 # infer the names in these same tweets
+# movie = imdb.IMBd()
 for t in nominee:
+    # print t
+    # print "\n"
     award = classify(t)
     # Recall: classify function returns None if no award matches
     if award:
+        # if (t == "Connie Britton should have won best actress #GoldenGlobes"):
+        #     print "AWARD: ", award
+        # print "AWARD_RELATED TWEET"
+        # print t
+        # print "\n"
+        # print award
+        # print "\n"
+        # print "\n"
         # print "Tweet: ", t
         # print "Award label: ", award
-        # if the award has to do with an actor...
-        if act_pat.search(award):
-            proper_nouns = pn2_pat.findall(t)
-            # print ("no....")
-            # print proper_nouns
-            proper_nouns = find_names_only(proper_nouns)
+
+        # if the award has to do with an actor / actress...
+        if act_pat.search(award) or director_pat.search(award):
+            proper_nouns = nominee_pat_1.findall(t)
+            for n in proper_nouns:
+                n = n[8:]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_2.findall(t)
+            for n in proper_nouns:
+                n = n[0:-14]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_3.findall(t)
+            for n in proper_nouns:
+                n = n[0:-11]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_4.findall(t)
+            for n in proper_nouns:
+                n = n[0:(n.find("did")-1)]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_5.findall(t)
+            for n in proper_nouns:
+                n = n[7:]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_6.findall(t)
+            for n in proper_nouns:
+                n = n[(n.find("been")+5):]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_7.findall(t)
+            for n in proper_nouns:
+                n = n[0:(n.find("should")-1)]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            if (all_nominee_pnouns[OFFICIAL_AWARDS.index(award)]):
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)] = list(set(all_nominee_pnouns[OFFICIAL_AWARDS.index(award)]))
+
         # if the award doesn't have to do with an actor...
         else:
-            proper_nouns = name_pattern.findall(t)
-            proper_nouns += name_with_lower.findall(t)
+            proper_nouns = nominee_pat_1.findall(t)
+            for n in proper_nouns:
+                n = n[8:]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_2.findall(t)
+            for n in proper_nouns:
+                n = n[0:-14]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_3.findall(t)
+            for n in proper_nouns:
+                n = n[0:-11]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_4.findall(t)
+            for n in proper_nouns:
+                n = n[0:(n.find("did")-1)]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_5.findall(t)
+            for n in proper_nouns:
+                n = n[7:]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_6.findall(t)
+            for n in proper_nouns:
+                n = n[(n.find("been")+5):]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            proper_nouns = nominee_pat_7.findall(t)
+            for n in proper_nouns:
+                n = n[0:(n.find("should")-1)]
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].insert(0,n)
+            if (all_nominee_pnouns[OFFICIAL_AWARDS.index(award)]):
+                all_nominee_pnouns[OFFICIAL_AWARDS.index(award)] = list(set(all_nominee_pnouns[OFFICIAL_AWARDS.index(award)]))
+
         # print "Pronouns found: ", proper_nouns
 
         # print "\n"
-        for n in proper_nouns:
-            all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].append(n)
+        # for n in proper_nouns:
+        #     all_nominee_pnouns[OFFICIAL_AWARDS.index(award)].append(n)
 
 print ("******************************************")
 correct = 0
-for i in range(len(all_nominee_pnouns)):
+for i in range(1,len(all_nominee_pnouns)):
     # print(OFFICIAL_AWARDS[i])
     # if Counter(all_nominee_pnouns[i]).most_common(3)[0][0].lower()==answers['award_data'][OFFICIAL_AWARDS[i]]['nominees']:
     #     correct += 1
     # else:
     print "Award: ", OFFICIAL_AWARDS[i]
-    print "Our answer: ", (Counter(all_nominee_pnouns[i]).most_common(10))  #aggregate PN lists using Counter and show top 3
+    # print "Our answer: ", (Counter(all_nominee_pnouns[i]).most_common(5))  #aggregate PN lists using Counter and show top 3
+    print "Our answer: ", all_nominee_pnouns[i][0:4]
     print "Desired answer:", (answers['award_data'][OFFICIAL_AWARDS[i]]['nominees']) #pull from answer key
-    print("------------------------------------------------")
+    print "\n"
 
 # print("Correctly extracting " + str(correct) + " of " + str(len(OFFICIAL_AWARDS)) + " awards")
